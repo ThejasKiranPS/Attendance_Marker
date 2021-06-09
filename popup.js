3da9af6411178c949691562bba7d373b38542a62
thejaskiranps <thejaskiranps@gmail.com>
2021-06-09 16:57:28 +0530

    add semesters for every year with animation

--- popup.js
+++ popup.js
@@ -1,15 +1,26 @@
 yearList=document.querySelector('.year-container').children;
 yearList= Array.from(yearList);
 
+function hide() {
+    yearList.forEach((e,i) => {
+        if(i%2==1) {
+            cList=e.classList;
+            if(cList[cList.length-1]=="flex") {
+            e.classList.toggle("flex");
+            }
+        }
+    })
+}
 function show() {
+    hide();
     year=this;
     year=parseInt(this.children[0].innerText)-1;
     semester=yearList[year*2+1];
-    semester.classList.toggle("flex")
+    semester.classList.toggle("flex");
 }
 
 yearList.forEach((e,i) => {
-    if (i==0) {
+    if (i%2==0) {
         e.onclick=show;
     }
 })
