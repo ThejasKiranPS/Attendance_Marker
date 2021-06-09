2dac0172569751b6cd7bd54d1d8193e96dd00f58
thejaskiranps <thejaskiranps@gmail.com>
2021-06-09 20:29:13 +0530

    remove old ui

--- popup.js
+++ popup.js
@@ -1,26 +1,2 @@
-yearList=document.querySelector('.year-container').children;
-yearList= Array.from(yearList);
-
-function hide() {
-    yearList.forEach((e,i) => {
-        if(i%2==1) {
-            cList=e.classList;
-            if(cList[cList.length-1]=="flex") {
-            e.classList.toggle("flex");
-            }
-        }
-    })
-}
-function show() {
-    hide();
-    year=this;
-    year=parseInt(this.children[0].innerText)-1;
-    semester=yearList[year*2+1];
-    semester.classList.toggle("flex");
-}
-
-yearList.forEach((e,i) => {
-    if (i%2==0) {
-        e.onclick=show;
-    }
-})
+//branch selector
+branchBox= document.querySelector(".branch-box");
