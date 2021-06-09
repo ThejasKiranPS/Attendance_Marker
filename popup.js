e5544c6ab3ae3955e32d82e8e9758a38774378f1
thejaskiranps <thejaskiranps@gmail.com>
2021-06-09 21:51:29 +0530

    add select feature

--- popup.js
+++ popup.js
@@ -1,2 +1,23 @@
-//branch selector
-branchBox= document.querySelector(".branch-box");
+branchList= Array.from(document.querySelector(".branch-box").children);
+semList=Array.from(document.querySelectorAll(".sem-box-container"));
+function selected() {
+    //clear
+    Array.from(this.parentElement.children).forEach((e,i) => {
+        if (e.classList[e.classList.length-1]=="selected") {
+            e.classList.toggle("selected");
+        }
+    })
+    this.classList.toggle("selected");
+}
+branchList.forEach(
+    (e,i) => {
+        e.onclick=selected;
+    }
+)
+semList.forEach(
+    (e,i) => {
+        Array.from(e.children).forEach((e,i) => {
+            e.onclick=selected;
+        })
+    }
+)
