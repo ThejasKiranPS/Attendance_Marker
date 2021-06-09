c2707b9901d4f9782912a976ee2e2cf7c65fee55
thejaskiranps <thejaskiranps@gmail.com>
2021-06-09 16:46:33 +0530

    add animation

--- popup.js
+++ popup.js
@@ -1,1 +1,15 @@
-year=document.querySelector('.year-container')
+yearList=document.querySelector('.year-container').children;
+yearList= Array.from(yearList);
+
+function show() {
+    year=this;
+    year=parseInt(this.children[0].innerText)-1;
+    semester=yearList[year*2+1];
+    semester.classList.toggle("flex")
+}
+
+yearList.forEach((e,i) => {
+    if (i==0) {
+        e.onclick=show;
+    }
+})
