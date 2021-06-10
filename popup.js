b6a0ded45cfa290727fb0eb9b4457e633db98aad
thejaskiranps <thejaskiranps@gmail.com>
2021-06-11 00:20:04 +0530

    move checkAttendance fn from popup to results

--- popup.js
+++ popup.js
@@ -37,25 +37,3 @@ function getData() {
     window.location.href='./upload.html';
 }
 document.querySelector("#submit").onclick=getData;
-
-async function checkAttendance() {
-    let [tab] = await chrome.tabs.query({active:true, currentWindow:true});
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        function: getParticipants,
-    });
-}
-function getParticipants() {
-    function listAll() {
-        pList=document.querySelector('[aria-label="Participants"]');
-        let list=[]
-        Array.from(pList.children).forEach((child,index) => {list.push(child.children[0].children[1].children[0].children[0].innerText);});
-        return list;
-    }
-    let data={};
-    data['participants']=listAll();
-    chrome.storage.sync.set(data);
-
-}
-
-checkAttendance();
---
+++ results.js
@@ -0,0 +1,19 @@
+async function checkAttendance() {
+    let [tab] = await chrome.tabs.query({active:true, currentWindow:true});
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        function: getParticipants,
+    });
+}
+function getParticipants() {
+    function listAll() {
+        pList=document.querySelector('[aria-label="Participants"]');
+        let list=[]
+        Array.from(pList.children).forEach((child,index) => {list.push(child.children[0].children[1].children[0].children[0].innerText);});
+        return list;
+    }
+    let data={};
+    data['participants']=listAll();
+    chrome.storage.sync.set(data);
+
+}
