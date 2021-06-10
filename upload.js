767729388a79bb3d517c06e9e8331dddf76d8049
thejaskiranps <thejaskiranps@gmail.com>
2021-06-10 18:35:54 +0530

    add fn for saving to local storage

---
+++ upload.js
@@ -0,0 +1,26 @@
+let fileName=localStorage.getItem("fileName");
+let dbName=fileName;
+let branchName= fileName.substring(0,fileName.length-2).toUpperCase();
+let semName= fileName.substring(fileName.length-2);
+fileName=' '+branchName+ ' ' + semName;
+document.querySelector(".fileName").innerText=fileName;
+
+document.getElementById('btn-upload').addEventListener('click', (e) => {
+    console.log('click');
+    e.preventDefault();
+    Papa.parse(document.getElementById('upload-csv').files[0], {
+        download:true,
+        header:false,
+        complete: (results) => {
+            console.log(results);
+            saveToLocal(results);
+        }
+    })
+});
+
+function saveToLocal(data){
+    let results={};
+    data['data'].shift();
+    results[dbName]=data['data']; 
+    chrome.storage.sync.set(results);
+}
